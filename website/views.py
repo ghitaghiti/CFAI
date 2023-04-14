f7192415e750d427e363029c7155c89d01451c57
Ghita Ghiti <ghitaghiti10@gmail.com>
2023-04-14 14:42:44 -0700

    add list of programing language in the select box

--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+    lang_list=['c', 'clike', 'cpp', 'csharp', 'css', 'go', 'html', 'java', 'javascript', 'julia', 'markup', 'php', 'python']
+
+    return render(request, 'home.html', {'lang_list': lang_list})
