16331151d5a12e87abd1af6442f640d42d5c8a65
Ghita Ghiti <ghitaghiti10@gmail.com>
2023-04-16 16:04:34 -0700

    delete test file, modification on : views.py , base.html, home.html .  creation of action between forms and alert .

--- website/views.py
+++ website/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.contrib import messages
+
 
 def home(request):
-    lang_list=['c', 'clike', 'cpp', 'csharp', 'css', 'go', 'html', 'java', 'javascript', 'julia', 'markup', 'php', 'python']
+    lang_list = ['c', 'clike', 'cpp', 'csharp', 'css', 'go', 'html',
+                 'java', 'javascript', 'julia', 'markup', 'php', 'python']
+    if request.method == 'POST':
+        code = request.POST['code']
+        lang = request.POST['lang']
 
+        if lang == "Select Programming Language":
+            messages.success(request,"You Forgot To Pick A Programming Language...")
+            return render(request, 'home.html', {'lang_list': lang_list, 'code': code, 'lang': lang})
+        return render(request, 'home.html', {'lang_list': lang_list, 'code': code, 'lang': lang})
+    
     return render(request, 'home.html', {'lang_list': lang_list})
+
