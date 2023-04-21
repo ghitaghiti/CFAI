7cb48ac7c2bcc54859e6062c952890895a993b45
Ghita Ghiti <ghitaghiti10@gmail.com>
2023-04-21 15:48:47 -0700

    modification on views,models,urls,db,past and navbar

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Code
+
+# Register Codes
+admin.site.register(Code)
+
+
 
 # Register your models here.
--- website/views.py
+++ website/views.py
@@ -4,19 +4,21 @@ import openai
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm
-# from .models import Code
+from .models import Code
 
 def home(request):
-	lang_list = ['c', 'clike', 'cpp', 'csharp', 'css', 'dart', 'django', 'go', 'html', 'java', 'javascript', 'markup', 'markup-templating', 'matlab', 'mongodb', 'objectivec', 'perl', 'php', 'powershell', 'python', 'r', 'regex', 'ruby', 'rust', 'sass', 'scala', 'sql', 'swift', 'yaml']
+	lang_list = ['c', 'clike', 'cpp', 'csharp', 'css', 'go', 'html', 'java', 'javascript', 'julia', 'markup', 'php', 'python']
 
 	if request.method == "POST":
 		code = request.POST['code']
 		lang = request.POST['lang']
 
+		# Check to make sure they picked a lang
 		if lang == "Select Programming Language":
 			messages.success(request, "Hey! You Forgot To Pick A Programming Language...")
 			return render(request, 'home.html', {'lang_list':lang_list, 'response':code, 'code':code, 'lang':lang})			
 		else:
+			# OpenAI Key
 			openai.api_key = ""
 			# Create OpenAI Instance
 			openai.Model.list()
@@ -50,7 +52,7 @@ def home(request):
 
 
 def suggest(request):
-	lang_list = ['c', 'clike', 'cpp', 'csharp', 'css', 'dart', 'django', 'go', 'html', 'java', 'javascript', 'markup', 'markup-templating', 'matlab', 'mongodb', 'objectivec', 'perl', 'php', 'powershell', 'python', 'r', 'regex', 'ruby', 'rust', 'sass', 'scala', 'sql', 'swift', 'yaml']
+	lang_list = ['c', 'clike', 'cpp', 'csharp', 'css', 'go', 'html', 'java', 'javascript', 'julia', 'markup', 'php', 'python']
 
 	if request.method == "POST":
 		code = request.POST['code']
@@ -132,24 +134,17 @@ def register_user(request):
 	return render(request, 'register.html', {"form": form})
 
 
-# def past(request):
-# 	if request.user.is_authenticated:
-# 		code = Code.objects.filter(user_id=request.user.id)
-# 		return render(request, 'past.html', {"code":code})	
-# 	else:
-# 		messages.success(request, "You Must Be Logged In To View This Page")
-# 		return redirect('home')
+def past(request):
+	if request.user.is_authenticated:
+		code = Code.objects.filter(user_id=request.user.id)
+		return render(request, 'past.html', {"code":code})	
+	else:
+		messages.success(request, "You Must Be Logged In To View This Page")
+		return redirect('home')
 
 
-# def delete_past(request, Past_id):
-# 	past = Code.objects.get(pk=Past_id)
-# 	past.delete()
-# 	messages.success(request, "Deleted Successfully...")
-# 	return redirect('past')
-# from django.shortcuts import redirect, render
-# from django.contrib import messages
-# import openai
-# from django.contrib.auth import authenticate,login,logout
-# from django.contrib.auth.forms import UserCreationForm
-# from .forms import SignUpForm
-
+def delete_past(request, Past_id):
+	past = Code.objects.get(pk=Past_id)
+	past.delete()
+	messages.success(request, "Deleted Successfully...")
+	return redirect('past')
